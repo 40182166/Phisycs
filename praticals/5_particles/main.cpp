67cffd6a331264c0acba04f5116ce6fe62f5c165
Scarfi <40182166@live.napier.ac.uk>
2016-11-04 11:50:16 +0000

    Spring created

--- praticals/5_particles/main.cpp
+++ praticals/5_particles/main.cpp
@@ -1,4 +1,4 @@
-#include "game.h"
+
 #include "physics.h"
 #include <glm/glm.hpp>
 #include <glm/gtx/rotate_vector.hpp>
@@ -10,31 +10,6 @@ using namespace std;
 using namespace graphics_framework;
 using namespace glm;
 
-//#ifndef SPRING_H
-//#define SPRING_H
-//
-//
-//class SPRING
-//{
-//public:
-//	//Indices of the balls at either end of the spring
-//	unique_ptr<Entity> ball1;
-//	unique_ptr<Entity> ball2;
-//
-//	//Tension in the spring
-//	float tension;
-//
-//	float constant;
-//	float length;
-//
-//	SPRING()
-//	{}
-//	~SPRING()
-//	{}
-//};
-//
-//#endif	//SPRING_H
-
 
 #define physics_tick 1.0 / 60.0
 
@@ -43,10 +18,9 @@ static unique_ptr<Entity> floorEnt;
 
 static vector<unique_ptr<Entity>> balls;
 
-//float springConstant = 15.0f;
-//float naturalLength = 1.0f;
+float springConstant = 2.0f;
+float naturalLength = 2.0f;
 
-//SPRING * spring;
 
 unique_ptr<Entity> CreateParticle() {
 	unique_ptr<Entity> ent(new Entity());
@@ -64,12 +38,23 @@ unique_ptr<Entity> CreateParticle() {
 
 void CreateSpring(const vector<unique_ptr<Entity>> &particles, float originalLength, float k)
 {
-	vec3 spring = particles[1]->GetPosition() - particles[0]->GetPosition();
-	auto length = glm::length(spring);
-	float displacement = length - originalLength;
+	vec3 force = particles[1]->GetPosition() - particles[0]->GetPosition();
+	float length = glm::length(force);
+	length = abs(length - originalLength) * k;
 
-	vec3 springNormal = spring / length;
-	vec3 restoreForce = springNormal * (displacement * k);
+	vec3 springNormal = normalize(force);
+	force = springNormal * (-length);
+	auto b = particles[0]->GetComponents("Physics");
+	if (b.size() == 1) {
+		const auto p = static_cast<cPhysics *>(b[0]);
+		p->forces -= force;
+	}
+
+	b = particles[1]->GetComponents("Physics");
+	if (b.size() == 1) {
+		const auto p = static_cast<cPhysics *>(b[0]);
+		p->forces += force;
+	}
 }
 
 
@@ -88,6 +73,8 @@ bool update(double delta_time) {
 		e->Update(delta_time);
 	}
 
+	CreateSpring(SceneList, naturalLength, springConstant);
+
 	phys::Update(delta_time);
 	return true;
 }
@@ -96,17 +83,9 @@ bool load_content() {
 	phys::Init();
 	for (size_t i = 0; i < 2; i++) {
 		SceneList.push_back(move(CreateParticle()));
-		balls.push_back(move(CreateParticle()));
 	}
 
-	CreateSpring(balls, 0.001f, 0.0001f);
-
-	/*spring = new SPRING;
-	spring->ball1 == balls[0];
-	spring->ball2 == balls[1];
-
-	spring->constant = springConstant;
-	spring->length = naturalLength;*/
+	CreateSpring(SceneList, naturalLength, springConstant);
 
 	floorEnt = unique_ptr<Entity>(new Entity());
 	floorEnt->AddComponent(unique_ptr<Component>(new cPlaneCollider()));
